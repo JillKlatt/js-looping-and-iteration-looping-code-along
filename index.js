7c0387f589270126054f2615614f8e41122ecf73
JillKlatt <klatt.jill@gmail.com>
2021-05-04 14:32:25 -0400

    commit tree 97d4d98f5cdb0e4b9f1cc17aa54390921d61ab10

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+        thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown( positiveInteger ) {
+    let countdown = positiveInteger;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
+
+countDown(10)
